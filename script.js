79d19defdc9d9dee0512ea4cebf2e1542f610b04
astra-we <emiv6442@gmail.com>
2026-06-03 06:55:44 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,46 @@
+const cards = document.querySelectorAll(".card");
+const stats = document.querySelectorAll(".stat");
+
+const observer = new IntersectionObserver(entries => {
+
+  entries.forEach(entry => {
+
+    if(entry.isIntersecting){
+
+      entry.target.style.opacity = "1";
+      entry.target.style.transform = "translateY(0)";
+
+    }
+
+  });
+
+},{
+  threshold:0.2
+});
+
+[...cards, ...stats].forEach(item => {
+
+  item.style.opacity = "0";
+  item.style.transform = "translateY(40px)";
+  item.style.transition = "0.6s ease";
+
+  observer.observe(item);
+
+});
+
 function whatsapp(plan){
-  let numero = "549XXXXXXXXXX";
-  let mensaje = "Hola, quiero el plan " + plan;
-  let url = "https://wa.me/" + numero + "?text=" + encodeURIComponent(mensaje);
+
+  const numero = "5493765205306";
+
+  const mensaje =
+  "Hola, quiero información sobre el plan " + plan;
+
+  const url =
+  "https://wa.me/" +
+  numero +
+  "?text=" +
+  encodeURIComponent(mensaje);
+
   window.open(url,"_blank");
+
 }
